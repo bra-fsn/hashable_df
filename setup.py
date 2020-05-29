8b05a52d43e27684d26a610d1494826f73be59c4
bra@fsn.hu <bra@fsn.hu>
2020-05-29 16:51:24 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,26 @@
+import io
+import re
+import setuptools
+
+with io.open("README.md", "rt", encoding="utf8") as f:
+    long_description = f.read()
+
+with io.open("autohash/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r"__version__ = \'(.*?)\'", f.read()).group(1)
+
+setuptools.setup(
+    name="hashable_df",
+    version=version,
+    author="NAGY, Attila",
+    author_email="nagy.attila@gmail.com",
+    description="Makes unhashable values in a pandas DataFrame hashable",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bra-fsn/hashable_df",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
