968e0ab95a30ae062d871d0c70b431d6a867754f
bra@fsn.hu <bra@fsn.hu>
2020-09-09 11:46:46 +0200

    Replace numpy.vectorize, which is faster, but has its limitations:       1. it supports maximum of 32 columns (ValueError: Cannot construct a          ufunc with more than 32 operands)       2. it samples the first row of each columns for the type and if that's          an np.nan, it will force float for the whole column.       3. #2 can be turned off by manually giving the otypes, but it might          fail on some types, like datetime:             TypeError: Cannot interpret 'datetime64[ns, UTC]' as a data type

--- hashable_df/__init__.py
+++ hashable_df/__init__.py
@@ -1,12 +1,19 @@
+"""Make DataFrame cells hashable."""
 import collections
-import numpy as np
 from autohash import AutoHash
 
 
+def f(value):
+    """Make value hashable."""
+    if isinstance(value, collections.abc.Hashable):
+        return value
+    else:
+        return AutoHash(value)
+
+
 def hashable_df(df):
-    return df.apply(np.vectorize(
-                    lambda value: value if isinstance(value, collections.abc.Hashable)
-                                        else AutoHash(value)))
+    """Return a DataFrame with hashable cell values."""
+    return df.applymap(f)
 
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
